79eb84aa99a8d6811d5fdc08f03a0f571d4cfbfe
storrellas <storrellas@gmail.com>
2020-07-08 00:29:38 +0200

    ENH: Added launch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
-
+"""
 print(" ")
 print(" ")
 print(" ")
@@ -29,7 +29,7 @@ print("----------------------------")
 output = decode(Image.open('Barcode3.jpg'))
 for item in output:
     print("code --> ", item.data)
-
+"""
 filename = 'foto3.jpg'
 print(" ")
 print(" ")
